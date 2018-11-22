41b9adccb206e76488f6b24a675b12064b5f445c
larry1010 <38902578+ll1010@users.noreply.github.com>
2018-11-22 00:21:34 -0500

    images folder added to src; image added to collapsible component

--- client/src/components/Module.js
+++ client/src/components/Module.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import Collapsible from 'react-collapsible';
+import React, { Component } from "react";
+import Collapsible from "react-collapsible";
+import image1 from '../images/spaghetti.jpg';
 
 // =================================================================================================================
 // https://www.npmjs.com/package/react-collapsible
@@ -9,29 +10,28 @@ class Module extends Component {
   render() {
     return (
       <div>
+        <hr />
         <br />
-        <Collapsible trigger="Spaghetti and Meatballs">
-          <ul>
-            <li>
-              <a href="https://www.foodnetwork.com/recipes/rachael-ray/spaghetti-and-meatballs-recipe-1942620">https://www.foodnetwork.com/recipes/rachael-ray/spaghetti-and-meatballs-recipe-1942620</a>
-            </li>
-          </ul>
+        <Collapsible trigger="Spaghetti and Meatballs [+]">
+        <img width={320} height={240} alt="spaghetti and meatballs" src={image1} />
+          <br />
+          <a href="https://www.foodnetwork.com/recipes/rachael-ray/spaghetti-and-meatballs-recipe-1942620">https://www.foodnetwork.com/recipes/rachael-ray/spaghetti-and-meatballs-recipe-1942620</a>
         </Collapsible>
         <br />
-        <Collapsible trigger="Ingredient List">
-
+        <Collapsible trigger="Ingredient List [+]">
           <ul>
             <li>1 pound spaghetti</li>
             <li>Salt, for pasta water</li>
           </ul>
-
           Meatballs:
           <ul>
             <li>1 1/4 pounds ground sirloin</li>
             <li>2 teaspoons Worcestershire sauce, eyeball it</li>
             <li>1 egg, beaten</li>
             <li>1/2 cup Italian bread crumbs, a couple of handfuls</li>
-            <li>1/4 cup grated Parmesan, Parmigiano-Reggiano or Romano cheese</li>
+            <li>
+              1/4 cup grated Parmesan, Parmigiano-Reggiano or Romano cheese
+            </li>
             <li>2 cloves garlic, chopped</li>
             <li>Salt and pepper</li>
           </ul>
@@ -41,28 +41,52 @@ class Module extends Component {
             <li>1/2 teaspoon crushed red pepper flakes</li>
             <li>4 cloves garlic, crushed or chopped</li>
             <li>1 small onion, finely chopped</li>
-            <li>1 cup beef stock, available on soup aisle in market in small paper boxes</li>
+            <li>
+              1 cup beef stock, available on soup aisle in market in small paper
+              boxes
+            </li>
             <li>1 (28-ounce) can crushed tomatoes</li>
             <li>A handful chopped flat-leaf parsley</li>
-            <li>Grated cheese, such as Parmigiano-Reggiano or Romano, for passing at table</li>
+            <li>
+              Grated cheese, such as Parmigiano-Reggiano or Romano, for passing
+              at table
+            </li>
             <li>10 leaves fresh basil leaves, torn or thinly sliced</li>
             <li>Crusty bread or garlic bread, for passing at the table</li>
           </ul>
         </Collapsible>
-
         <br />
-        <Collapsible trigger="Instructions">
+        <Collapsible trigger="Instructions [+]">
           <ol>
             <li>Preheat oven to 425 degrees F.</li>
-            <li>Place a large pot of water on to boil for spaghetti. When it boils, add salt and pasta and cook to al dente.</li>
-            <li>Mix beef and Worcestershire, egg, bread crumbs, cheese, garlic, salt and pepper. Roll meat into 1 1/2 inch medium-sized meatballs and place on nonstick cookie sheet or a cookie sheet greased with extra-virgin olive oil. Bake balls 10 to 12 minutes, until no longer pink.</li>
-            <li>Heat a deep skillet or medium pot over moderate heat. Add oil, crushed pepper, garlic and finely chopped onion. Saute 5 to 7 minutes, until onion bits are soft. Add beef stock, crushed tomatoes, and herbs. Bring to a simmer and cook for about 10 minutes.</li>
-            <li>Toss hot, drained pasta with a few ladles of the sauce and grated cheese. Turn meatballs in remaining sauce. Place pasta on dinner plates and top with meatballs and sauce and extra grated cheese. Serve with bread or garlic bread (and some good chianti!)</li>
+            <li>
+              Place a large pot of water on to boil for spaghetti. When it
+              boils, add salt and pasta and cook to al dente.
+            </li>
+            <li>
+              Mix beef and Worcestershire, egg, bread crumbs, cheese, garlic,
+              salt and pepper. Roll meat into 1 1/2 inch medium-sized meatballs
+              and place on nonstick cookie sheet or a cookie sheet greased with
+              extra-virgin olive oil. Bake balls 10 to 12 minutes, until no
+              longer pink.
+            </li>
+            <li>
+              Heat a deep skillet or medium pot over moderate heat. Add oil,
+              crushed pepper, garlic and finely chopped onion. Saute 5 to 7
+              minutes, until onion bits are soft. Add beef stock, crushed
+              tomatoes, and herbs. Bring to a simmer and cook for about 10
+              minutes.
+            </li>
+            <li>
+              Toss hot, drained pasta with a few ladles of the sauce and grated
+              cheese. Turn meatballs in remaining sauce. Place pasta on dinner
+              plates and top with meatballs and sauce and extra grated cheese.
+              Serve with bread or garlic bread (and some good chianti!)
+            </li>
           </ol>
         </Collapsible>
-
         <br />
-        <Collapsible trigger="Discussion">
+        <Collapsible trigger="Discussion [+]">
           <ul>
             <li>This recipe is AMAZING!!!</li>
             <li>I can eat this every night!!!</li>
